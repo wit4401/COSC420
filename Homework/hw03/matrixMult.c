ffb4c177775ca1f29cc2338f958b00b20f0f1c8d
wit4401 <wtownsend4401@gmail.com>
2022-09-23 10:08:05 -0400

    added threadingEx in hw02 and added matrixMult.c

---
+++ Homework/hw03/matrixMult.c
@@ -0,0 +1,11 @@
+/*
+ * Homework_03 (Matrix Multiplication w/ pthreads)
+ *
+ * This program uses the c pthread library to speed
+ * up matrix multiplication in parallell.
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+#include<pthread.h>
