690ee2cef1bff3b20be5ca57e365ccd5840dd5a8
wit4401 <wit4401@gmail.com>
2022-09-30 09:56:40 -0400

    updated serialMatrixMult.c for user input

--- Homework/hw03/serialMatrixMult.c
+++ Homework/hw03/serialMatrixMult.c
@@ -1,25 +1,58 @@
+/*
+ * Serial nxn Matrix Multiplication Program
+ * to compare to the assigned parallel
+ * version of the program
+*/
+
 #include<stdio.h>
+#include<time.h>
 #include<stdlib.h>
 
 int main(int argc, char *args[]){
-    int A[2][2] = {1, 2, 
-                   3, 4};
+    int n;
+    printf("Enter a value n: ");
+    scanf("%d ",&n);
 
-    int B[2][2] = {1, 2,
-                   3, 4};
-    
-    int result[2][2]={0,0,
-                      0,0};
-                      
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-            for(int k=0;k<2;k++)
-                result[i][j] += A[i][k]*B[k][j];
+    int matA[n][n];
+    int matB[n][n];
+    int matResult[n][n];
+    srand(time(NULL));
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++)
+            matResult[i][j]=0;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            matA[i][j]=rand()%11;
+            matB[i][j]=rand()%11;
         }
     }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            for(int k=0;k<n;k++)
+                matResult[i][j] += matA[i][k]*matB[k][j];
+        }
+    }
+    puts("Matrix A:");
     for(int i=0;i<2;i++){
         for(int j=0;j<2;j++){
-            printf("%d ",result[i][j]);
+            printf("%d ",matA[i][j]);
+        }
+        puts("");
+    }
+    puts("Matrix B:");
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            printf("%d ",matB[i][j]);
+        }
+        puts("");
+    }
+    puts("AxB:");
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            printf("%d ",matResult[i][j]);
         }
         puts("");
     }
