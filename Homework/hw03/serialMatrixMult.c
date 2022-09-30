489a37e11091da9a453da5920881f3a9fd776cc8
wit4401 <wtownsend4401@gmail.com>
2022-09-30 12:07:54 -0500

    working on mxn matricies

--- Homework/hw03/serialMatrixMult.c
+++ Homework/hw03/serialMatrixMult.c
@@ -9,49 +9,64 @@
 #include<stdlib.h>
 
 int main(int argc, char *args[]){
-    int n;
-    printf("Enter a value n: ");
-    scanf("%d ",&n);
+    int rows1,columns1,rows2,columns2;
+    printf("Enter # of rows (Matrix 1): ");
+    scanf("%d",&rows1);
+    printf("Enter # of columns (Matrix 2): ");
+    scanf("%d",&columns1);
 
-    int matA[n][n];
-    int matB[n][n];
-    int matResult[n][n];
+    printf("Enter # of rows (Matrix 1): ");
+    scanf("%d",&rows2);
+    printf("Enter # of columns (Matrix 2): ");
+    scanf("%d",&columns2);
+
+    if(rows2!=columns1){
+	puts("Cannot Multipy with given values.");
+	exit(1);
+    }
+
+    int matA[rows1][columns1];
+    int matB[rows2][columns2];
+    int matResult[rows1][columns2];
     srand(time(NULL));
 
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++)
             matResult[i][j]=0;
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=0;i<columns1;i++){
+        for(int j=0;j<columns1;j++)
             matA[i][j]=rand()%11;
-            matB[i][j]=rand()%11;
-        }
     }
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            for(int k=0;k<n;k++)
+    for(int i=0;i<rows2;i++){
+        for(int j=0;j<columns2;j++)
+            matB[i][j]=rand()%11;
+    }
+
+    for(int i=0;i<rows1;i++){
+        for(int j=0;j<columns2;j++){
+            for(int k=0;k<columns2;k++)
                 matResult[i][j] += matA[i][k]*matB[k][j];
         }
     }
     puts("Matrix A:");
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
+    for(int i=0;i<rows1;i++){
+        for(int j=0;j<columns1;j++){
             printf("%d ",matA[i][j]);
         }
         puts("");
     }
-    puts("Matrix B:");
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
+    puts("\nMatrix B:");
+    for(int i=0;i<rows2;i++){
+        for(int j=0;j<columns2;j++){
             printf("%d ",matB[i][j]);
         }
         puts("");
     }
-    puts("AxB:");
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
+    puts("\nAxB:");
+    for(int i=0;i<rows1;i++){
+        for(int j=0;j<columns2;j++){
             printf("%d ",matResult[i][j]);
         }
         puts("");
