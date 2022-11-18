7f1f905518093eb81bb56652fb9d7b530b0d8837
wit4401 <wtownsend4401@gmail.com>
2022-11-18 18:33:07 -0500

    moved backward original plan not going to work

--- Program2/socialNetwork.java
+++ Program2/socialNetwork.java
@@ -19,48 +19,58 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class socialNetwork {
-    public static class NodeMapper extends Mapper<Object,Text,Text,Text>{
+    public static class MapperOne extends Mapper<Object,Text,Text,Text>{
         public void map(Object obj,Text friends,Context context)throws IOException, InterruptedException{
             String[] list = friends.toString().split(" ",0);
             for (int i=1;i<list.length;i++){
-                context.write(new Text(list[0]), new Text(list[i]));
+                if(list[0]<list[i])
+                    context.write(new Text(list[0]), new Text(list[i]));
             }
         }
     }
 
-    public static class PairMapper extends Mapper<Object,Text,Text,Text>{
+    public static class MapperTwo extends Mapper<Object,Text,Text,Text>{
         public void map(Object key, Text friends,Context context)throws IOException, InterruptedException{
-            String[] list = friends.toString().split(" ",0);
-            String[] pairs = new String[2];
-            for(int i=1; i<list.length;i++){
-                for(int j=i+1;i<list.length-1;j++){
-                    if (list[i].equals(list[j])==false){
-                        String pair = list[i] + " " + list[j];
-                        pairs[0] = list[0] + " " + list[i];
-                        pairs[1] = list[0] + " " + list[j];
-                        context.write(new Text(pair),new Text(pairs[0] + " " + pairs[1]));
-                    }
-                }
-            }
+
         }
     }
 
-    public static class TripletReducer extends Reducer<Text,Text,Text,Text>{
-        public void reduce(IntWritable key,IntWritable value,Context context)throws IOException, InterruptedException{
-            context.write(key,value);
+    public static class ReducerOne extends Reducer<Text,Text,Text,Text>{
+        public void reduce(Text key,Text value,Context context)throws IOException, InterruptedException{
+            
+        }
+    }
+
+    public static class ReducerTwo extends Reducer<Text,Text,Text,Text>{
+        public void reduce(Text key,Text value,Context context)throws IOException, InterruptedException{
+            
         }
     }
 
     public static void main(String[] args)throws Exception{
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "social network");
-        job.setJarByClass(socialNetwork.class);
-        job.setMapperClass(PairMapper.class);
-        job.setReducerClass(TripletReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+        Job job1 = Job.getInstance(conf, "social network1");
+        
+        job1.setJarByClass(socialNetwork.class);
+        job1.setMapperClass(MapperOne.class);
+        job1.setReducerClass(ReducerOne.class);
+        job1.setOutputKeyClass(Text.class);
+        job1.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job1, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job1, new Path(args[1]));
+
+        Job job2 = Job.getInstance(conf, "social network2");
+        job2.setJarByClass(socialNetwork.class);
+        job2.setMapperClass(MapperTwo.class);
+        job2.setReducerClass(ReducerTwo.class);
+        job2.setOutputKeyClass(Text.class);
+        job2.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job2, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job2, new Path(args[1]));
+        
+        System.exit(job1.waitForCompletion(true) ? 0 : 1);
+        System.exit(job2.waitForCompletion(true) ? 0 : 1);
+
     }
 }
