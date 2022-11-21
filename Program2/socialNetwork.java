8badc348a24c05f217c832d5c6a483735a66c20d
wit4401 <wtownsend4401@gmail.com>
2022-11-21 00:32:54 -0500

    finished coding Program 2 (untested)

--- Program2/socialNetwork.java
+++ Program2/socialNetwork.java
@@ -12,6 +12,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -19,31 +20,35 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class socialNetwork {
-    public static class MapperOne extends Mapper<Object,Text,Text,Text>{
+    public static class PairMapper extends Mapper<Object,Text,Text,Text>{
         public void map(Object obj,Text friends,Context context)throws IOException, InterruptedException{
             String[] list = friends.toString().split(" ",0);
-            for (int i=1;i<list.length;i++){
-                if(list[0]<list[i])
-                    context.write(new Text(list[0]), new Text(list[i]));
+            String[] pairs = new String[2];
+            for(int i=1; i<list.length;i++){
+                for(int j=i+1;j<list.length;j++){
+                    String pair = list[i] + " " + list[j];
+                    pairs[0] = list[0] + " " + list[i];
+                    pairs[1] = list[0] + " " + list[j];
+                    context.write(new Text(pair + "=" + pairs[0] + " " + pairs[1]), new Text(friends));
+                }
             }
         }
     }
 
-    public static class MapperTwo extends Mapper<Object,Text,Text,Text>{
-        public void map(Object key, Text friends,Context context)throws IOException, InterruptedException{
-
-        }
-    }
-
-    public static class ReducerOne extends Reducer<Text,Text,Text,Text>{
-        public void reduce(Text key,Text value,Context context)throws IOException, InterruptedException{
-            
-        }
-    }
-
-    public static class ReducerTwo extends Reducer<Text,Text,Text,Text>{
-        public void reduce(Text key,Text value,Context context)throws IOException, InterruptedException{
-            
+    public static class TripletReducer extends Reducer<Text,Text,Text,NullWritable>{
+        public void reduce(Text key,Iterable<Text> values,Context context)throws IOException, InterruptedException{
+            String[] edgeRelation = new key.toString().split("=",0);
+            String compare = edgeRelation[0];
+            String userId = edgeRelation[1].substring(1,2);
+            for (Text list : values){
+                String[] friendList = list.toString().split(" ",0);
+                String edge;
+                for (int i=0;i<list.length-1;i++){
+                    edge=friendList[0] + " " + friendList[i];
+                    if(compare.equals(edge))
+                        context.write(new Text("<"+userId+","+friendList[0]+","+friendList[i]+">"),null);
+                }
+            }
         }
     }
 
@@ -51,26 +56,15 @@ public class socialNetwork {
         Configuration conf = new Configuration();
 
         Job job1 = Job.getInstance(conf, "social network1");
-        
         job1.setJarByClass(socialNetwork.class);
-        job1.setMapperClass(MapperOne.class);
-        job1.setReducerClass(ReducerOne.class);
+        job1.setMapperClass(PairMapper.class);
+        job1.setReducerClass(TripletReducer.class);
         job1.setOutputKeyClass(Text.class);
-        job1.setOutputValueClass(Text.class);
+        job1.setOutputValueClass(NullWritable.class);
         FileInputFormat.addInputPath(job1, new Path(args[0]));
         FileOutputFormat.setOutputPath(job1, new Path(args[1]));
-
-        Job job2 = Job.getInstance(conf, "social network2");
-        job2.setJarByClass(socialNetwork.class);
-        job2.setMapperClass(MapperTwo.class);
-        job2.setReducerClass(ReducerTwo.class);
-        job2.setOutputKeyClass(Text.class);
-        job2.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job2, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job2, new Path(args[1]));
         
         System.exit(job1.waitForCompletion(true) ? 0 : 1);
-        System.exit(job2.waitForCompletion(true) ? 0 : 1);
 
     }
 }
