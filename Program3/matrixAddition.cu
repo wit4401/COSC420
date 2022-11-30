2d0acd4607acb8f067384e7e4c6b7145e89b2a58
wit4401 <wtownsend4401@gmail.com>
2022-11-30 11:14:19 -0500

    addition done?

--- Program3/matrixAddition.cu
+++ Program3/matrixAddition.cu
@@ -4,12 +4,24 @@
 #include<cuda.h>
 #include<cuda_runtime.h>
 #include<time.h>
-#define SIZE 2500
+#define SIZE 25
 
 __global__ void matrix_addition(int *a,int *b, int *res,int n){
     int i = threadIdx.x;
     if (i<n)
-        res[i] = a[i]+b[i]
+        res[i] = a[i]+b[i];
+}
+
+void printMatrix(int *mat, int len_row){
+    int multiple=1;
+    for (int i=0;i<SIZE;i++){
+        if(i == (multiple*len_row)){
+            printf("%d\n",mat[i]);
+            multiple++;
+        }
+        else
+            printf("%d ",mat[i]);
+    }
 }
 
 int main(int argc, char **argv){
@@ -27,10 +39,18 @@ int main(int argc, char **argv){
     }
 
     matrix_addition<<<1,1>>>(matA,matB,result,SIZE);
+    cudaDeviceSynchronize();
+
+    len=sqrt(SIZE);
+    printMatrix(matA,SIZE,len);
+    puts("Plus");
+    printMatrix(matB,SIZE,len);
+    puts("Equals");
+    printMatrix(result,SIZE,len);
 
     cudaFree(matA);
     cudaFree(matB);
     cudaFree(result);
     
-    return 0;
+    exit(0);
 }
