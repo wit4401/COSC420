dc670b9fbe1150b9cf3f549c0ed29211ce14ea5c
wit4401 <wtownsend4401@gmail.com>
2022-11-30 09:58:29 -0500

    addition cuda done?

--- Program3/matrixAddition.cu
+++ Program3/matrixAddition.cu
@@ -3,18 +3,28 @@
 #include<cuda.h>
 #include<cuda_runtime.h>
 #include<time.h>
-#define SIZE 1024
+#define SIZE 2500
 
-__global__ void matrix_addition(int *a,int *b, int *res){
-
+__global__ void matrix_addition(int *a,int *b, int *res,int n){
+    int i = threadIdx.x;
+    if (i<n)
+        res = a[i]+b[i]
+    
 }
 
 int main(int argc, char **argv){
     int *matA,*matB,*result;
+    srand(time(NULL));
 
     cudaMalloc(&matA,sizeof(int)*SIZE);
     cudaMalloc(&matB,sizeof(int)*SIZE);
     cudaMalloc(&result,sizeof(int)*SIZE);
+
+    for(int i=0;i<SIZE;i++){
+        matA[i]=rand()%101;
+        matB[i]=rand()%101;
+        result[i]=0;
+    }
 
     matrix_addition<<<1,1>>>(matA,matB,result);
 
