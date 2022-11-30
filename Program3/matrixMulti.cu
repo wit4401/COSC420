585efabfac818e382d763a07efa039eba4d6c9c2
wit4401 <wtownsend4401@gmail.com>
2022-11-30 13:38:15 -0500

    added setup for multiplication

--- Program3/matrixMulti.cu
+++ Program3/matrixMulti.cu
@@ -7,21 +7,58 @@
 #include<cuda.h>
 #include<cuda_runtime.h>
 #include<time.h>
-#define SIZE 1024
+#define SIZE 10000
+#define GRID 10
 
-__global__ void matrix_multiply(int *a,int *b, int *res){
+__global__ void matrix_multiply(int *a,int *b, int *res, int n){
 
+}
+
+/* Prints out the given matrix */
+void printMatrix(int *mat, int len_row){
+    int multiple=1;
+    for (int i=0;i<SIZE;i++){
+        if(i == (multiple*len_row)-1){
+            printf("%d\n",mat[i]);
+            multiple++;
+        }
+        else
+            printf("%d ",mat[i]);
+    }
 }
 
 int main(int argc, char **argv){
     int *matA,*matB,*result;
+    int len=sqrt(SIZE);/* length of each row/column */
+    srand(time(NULL));
 
+    //allocate appropriate memory to each dynamic array
     cudaMalloc(&matA,sizeof(int)*SIZE);
     cudaMalloc(&matB,sizeof(int)*SIZE);
     cudaMalloc(&result,sizeof(int)*SIZE);
 
-    matrix_multiply<<<1,1>>>(matA,matB,result);
+    // assigns random number to each index of the dynamic arrays to be added
+    for(int i=0;i<SIZE;i++){
+        matA[i]=rand()%101;
+        matB[i]=rand()%101;
+    }
 
+    dim3 grid_size(GRID); //stores our grid dimensions
+    dim3 block_size(SIZE/GRID);//stores our block dimensions
+
+    //call the kernel with the appropriate grid and block dimensions
+    matrix_multiply<<<grid_size,block_size>>>(matA,matB,result,SIZE);
+    cudaDeviceSynchronize();
+
+    //print out results from computation
+    printMatrix(matA,len);
+    puts("\nPlus");
+    printMatrix(matB,len);
+    puts("\nEquals");
+    printMatrix(result,len);
+    puts("");
+
+    //clean up memory
     cudaFree(matA);
     cudaFree(matB);
     cudaFree(result);
