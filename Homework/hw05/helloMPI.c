6c1e35328ee17073423cf3d025d200fcac84d667
wit4401 <wtownsend4401@gmail.com>
2022-12-01 10:49:57 -0500

    finished helloMPI

--- Homework/hw05/helloMPI.c
+++ Homework/hw05/helloMPI.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include <mpi.h>
 
-int main(){
+int main(int argc,char **argv){
     int rank;//place holder for the processor #
 
-    MPI_Init(); // initializes the processors
+    MPI_Init(NULL,NULL); // initializes the processors
     MPI_Comm_rank(MPI_COMM_WORLD,&rank); // retrieves the rank of the current process
-    printf("Hello World from Processor %d",rank); // each processor prints out basic message
+    printf("Hello World from Processor %d\n",rank); // each processor prints out basic message
     MPI_Finalize(); // stops/merges processors back to main CPU
     exit(0);
 }
