410c6f7ffbc1ab81a7292a7e1789c10739acd9c0
wit4401 <wtownsend4401@gmail.com>
2022-12-03 16:07:12 -0500

    slight mods to output

--- Project/tsp_serial_bf.cpp
+++ Project/tsp_serial_bf.cpp
@@ -7,12 +7,11 @@
 #include<iostream>
 #include<algorithm>
 #include<climits>
+#include<ctime>
 #include<vector>
 #include<stdlib.h>
-#include<time.h>
-using namespace std;
 
-int vertices;
+using namespace std;
 
 void tsp(int **graph, int start_vertex, int n){
     vector<int> v;
@@ -22,23 +21,35 @@ void tsp(int **graph, int start_vertex, int n){
     }
 
     int minimum = INT_MAX;
-    
+    vector<int> result;
     while(next_permutation(v.begin(),v.end())){
         int curr_vert=start_vertex-1;
         int curr_weight=0;
+        vector<int> temp;
+        temp.push_back(start_vertex);
 
         for(int i=0;i<v.size();i++){
+            temp.push_back(v[i]+1);
             curr_weight+=graph[curr_vert][v[i]];
             curr_vert=v[i];
         }
         curr_weight+=graph[curr_vert][start_vertex-1];
         minimum=min(minimum,curr_weight);
+
+        if(minimum==curr_weight)
+            result=temp;
+
     }
-    cout<<"Minimum Path: "<<minimum<<endl;
+
+    cout<<"Minimum Path: ";
+    for(int i=0;i<result.size();i++)
+        cout<<result[i]<<" -> ";
+    cout<<start_vertex<<" ("<<minimum<<")"<<endl;
 }
 
 int main(int argc, char **argv){
-    int start;
+    clock_t begin,end;
+    int start,vertices;
     srand(time(0));
     
     cout<<"Verticies for random graph (n): ";
@@ -47,6 +58,7 @@ int main(int argc, char **argv){
     cout<<"Starting Node (1 to n): ";
     cin>>start;
 
+    cout<<endl;
     int **graph = new int*[vertices];
 
     for(int i=0;i<vertices;i++){
@@ -65,9 +77,12 @@ int main(int argc, char **argv){
         }
         cout<<endl;
     }
-
     cout<<endl;
+
+    begin=clock();
     tsp(graph,start,vertices);
+    end=clock();
+    cout<<"tsp() Elapsed Time: "<<(double)(end-begin)/CLOCKS_PER_SEC<<" sec"<<endl;
 
     return 0;
 }
